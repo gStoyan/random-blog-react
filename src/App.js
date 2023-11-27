77d66b1fbe9ebd5734743a17343ca47f97adf408
gStoyan <grancharovstoyan@gmail.com>
2023-11-27 12:41:22 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
 }
 
 export default App;
